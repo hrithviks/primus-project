be1bb626b06f2f71d81e3759ef8d60d91ffaa6b0
Hrithvik S <hrithsnair90@gmail.com>
2026-01-10 01:33:08 +0800

    Add root configuration files for Observability App

---
+++ terraform/observability/00-outputs.tf
@@ -0,0 +1,47 @@
+/*
+ * Script Name: outputs.tf
+ * Project Name: Primus
+ * Description: Exposes key infrastructure attributes as Terraform outputs.
+ * These values are intended for use by downstream systems, CI/CD pipelines,
+ * or for operator reference.
+ */
+
+output "ecs_cluster_id" {
+  description = "The unique identifier of the ECS cluster."
+  value       = aws_ecs_cluster.main.id
+}
+
+output "ecs_cluster_name" {
+  description = "The name of the ECS cluster."
+  value       = aws_ecs_cluster.main.name
+}
+
+output "vpc_id" {
+  description = "The ID of the VPC where the cluster is deployed."
+  value       = module.main_vpc.vpc_id
+}
+
+output "opensearch_secret_arn" {
+  description = "The ARN of the Secrets Manager secret containing the OpenSearch initial admin password."
+  value       = aws_secretsmanager_secret.opensearch_admin_password.arn
+}
+
+output "opensearch_endpoint" {
+  description = "The public HTTP endpoint for the OpenSearch API."
+  value       = "http://${module.app_load_balancer.alb_dns_name}:9200"
+}
+
+output "dashboards_endpoint" {
+  description = "The public URL for the OpenSearch Dashboards user interface."
+  value       = "http://${module.app_load_balancer.alb_dns_name}/dashboards"
+}
+
+output "logstash_endpoint" {
+  description = "The public HTTP endpoint for Logstash ingestion."
+  value       = "http://${module.app_load_balancer.alb_dns_name}:9600"
+}
+
+output "logstash_ingest_endpoint" {
+  description = "The public HTTP endpoint for sending logs to Logstash."
+  value       = "http://${module.app_load_balancer.alb_dns_name}:8080"
+}
